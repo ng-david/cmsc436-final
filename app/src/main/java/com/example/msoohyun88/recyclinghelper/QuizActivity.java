e7f8554083ecf3cfdd2fc8a91753fe57e73bd6a7
AyeAndy <msoohyun88@gmail.com>
2018-12-01 00:51:58 -0500

    quiz_activity created

---
+++ app/src/main/java/com/example/msoohyun88/recyclinghelper/QuizActivity.java
@@ -0,0 +1,14 @@
+package com.example.msoohyun88.recyclinghelper;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class QuizActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+    }
+
+}
