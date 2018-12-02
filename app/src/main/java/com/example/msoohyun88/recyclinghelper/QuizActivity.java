445b8116fb6cf5548d0f9c2d16331a47c8ef7d5d
AyeAndy <msoohyun88@gmail.com>
2018-12-01 20:01:56 -0500

    yes

--- app/src/main/java/com/example/msoohyun88/recyclinghelper/QuizActivity.java
+++ app/src/main/java/com/example/msoohyun88/recyclinghelper/QuizActivity.java
@@ -2,73 +2,85 @@ package com.example.msoohyun88.recyclinghelper;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.msoohyun88.recyclinghelper.database.Item;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Random;
+
+import static com.example.msoohyun88.recyclinghelper.ProgressFragment.RESULT_KEY;
 
 public class QuizActivity extends AppCompatActivity {
 
     private final static int nQuizQuestions = 5;
-    private RadioGroup mRadioGroup;
+
+    private Button mTrashButton;
+    private Button mRecycleButton;
+    private Button mCompostButton;
+
     private ImageView mImageView;
-    private Button mNextButton;
     private TextView mItemName;
     private int numberCorrect;
     private int questionNumber;
     private ArrayList<Item> itemList;
-    private HashMap<String, String> mAnswerKey = new HashMap<String, String>(); //Item name to item category
+    private ArrayList<Item> chosenItemList = new ArrayList<Item>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
-        mRadioGroup = (RadioGroup) findViewById(R.id.radio_group);
+        mTrashButton = (Button) findViewById(R.id.trash);
+        mRecycleButton = (Button) findViewById(R.id.recyclable);
+        mCompostButton = (Button) findViewById(R.id.compost);
+
+        int primaryColor = getColor(R.color.colorPrimary);
+        int greenColor = getColor(R.color.colorGreen);
+        int grayColor = getColor(R.color.colorGray);
+        int grey = getColor(R.color.colorWhite);
+
+        mTrashButton.setBackgroundColor(grayColor);
+        mTrashButton.setTextColor(grey);
+        mRecycleButton.setBackgroundColor(primaryColor);
+        mRecycleButton.setTextColor(grey);
+        mCompostButton.setBackgroundColor(greenColor);
+        mCompostButton.setTextColor(grey);
+
         mImageView = (ImageView) findViewById(R.id.material_image);
-        mNextButton = (Button) findViewById(R.id.next_button);
         mItemName = (TextView) findViewById(R.id.item_name);
+        itemList = new ArrayList<Item>();
 
         numberCorrect = 0;
         questionNumber = 0;
 
-        mNextButton.setOnClickListener(new View.OnClickListener() {
+        View.OnClickListener buttonClickListeners = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                String answer = "";
-
-                switch (mRadioGroup.getCheckedRadioButtonId()) {
-                    case 0:
-                        answer = "trash";
-                        break;
-                    case 1:
-                        answer = "recyclable";
-                        break;
-                    case 2:
-                        answer = "compost";
-                }
-
-                if (answer == "") {
-                    Toast.makeText(getApplicationContext(), "Please choose an answer", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-
-                mItemName.setText(itemList.get(questionNumber).getName());
-                String name = mItemName.getText().toString();
+                String answer = ((Button)v).getText().toString().toLowerCase();
 
                 //checks the answer key to make sure the answer is correct
-                if (mAnswerKey.get(name).equals(answer)) {
+                if (chosenItemList.get(questionNumber).getCategory().equals(answer)) {
                     numberCorrect++;
                 }
 
@@ -79,44 +91,83 @@ public class QuizActivity extends AppCompatActivity {
 
                     //returning data to original main activity
                     Intent intent = new Intent();
-                    intent.putExtra("NUM_CORRECT", numberCorrect);
+                    intent.putExtra(RESULT_KEY, numberCorrect);
                     setResult(RESULT_OK, intent);
                     finish();
 
                 } else {
 
                     //prepares next question and clears the check.
-                    mItemName.setText(itemList.get(questionNumber).getName());
-                    mRadioGroup.clearCheck();
+                    mItemName.setText(chosenItemList.get(questionNumber).getName());
                 }
-
             }
-        });
+        };
 
-        create_start_quiz();
+        mTrashButton.setOnClickListener(buttonClickListeners);
+        mRecycleButton.setOnClickListener(buttonClickListeners);
+        mCompostButton.setOnClickListener(buttonClickListeners);
 
-        finish();
+        setUpFirebaseListeners();
     }
 
-    private void get_data() {
-        //gets the data from firebase here. Choose random trash
-        itemList = new ArrayList<Item>();
+    private void setUpFirebaseListeners() {
+        // Get reference to the loading activityIndicator
+        final RelativeLayout activityIndicator = findViewById(R.id.activityIndicator);
+
+        // Communicate with FB
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference itemRef = database.getReference("items");
+        itemRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                // Populate itemList with Items
+                HashMap<String, ArrayList<Map<String, String>>> map = (HashMap) dataSnapshot.getValue();
+                for (String k : map.keySet()) {
+                    ArrayList<Map<String, String>> currList = map.get(k);
+                    for (Map<String, String> itemMap : currList) {
+                        String category = k.equals("trash") ? "trash" : k.equals("compost") ? "compost" : "recycle";
+                        Item item = new Item(itemMap.get("name"), itemMap.get("details"), category);
+                        itemList.add(item);
+                    }
+                }
+
+                // If we have data, hide the activityIndicator
+                activityIndicator.setVisibility(View.GONE);
+
+                create_start_quiz();
+
+                Log.w("QUIZ_ACTIVITY", "Successfully updated local list");
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+
+                activityIndicator.setVisibility(View.GONE);
+                Log.w("QUIZ_ACTIVITY", "Failed to read value.", error.toException());
+            }
+        });
     }
 
     private void create_start_quiz(){
 
         //goes through the
         //generate random fake data
-        get_data();
 
-        for (int i = 0; i < nQuizQuestions; i++) {
+        Random rand = new Random();
+        int listSize = itemList.size();
+        HashSet<Integer> indices = new HashSet<Integer>();
 
-            //choose random data and populate item list with it
+        while(indices.size() < nQuizQuestions) {
+            indices.add(rand.nextInt(listSize));
+        }
+
+        for (int current: indices) {
+            chosenItemList.add(itemList.get(current));
         }
 
         //set the item name set to the first item in the list
-
-        //mItemName.setText(itemList.get(0).getName());
+        mItemName.setText(chosenItemList.get(0).getName());
 
     }
 }
